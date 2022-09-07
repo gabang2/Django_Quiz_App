0e9c93d8ee045454f5552d5ebdc8f2d8c1ac2298
gabang2 <offbeat1020@naver.com>
2022-09-07 13:24:38 +0700

    random 하게 퀴즈 나오도록 설정 완료

--- quiz/views.py
+++ quiz/views.py
@@ -2,8 +2,17 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Quiz
 from .serializers import QuizSerializer
+import random
+
 
 @api_view(['GET'])
 def helloAPI(request):
     return Response('helo_world!')
 
+
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizs = Quiz.objects.all()
+    randomQuiz = random.sample(list(totalQuizs), id)
+    serializer = QuizSerializer(randomQuiz, many=True)  # many=True로 하면 다량의 데이터도 직렬화를 수행해줌..!!!
+    return Response(serializer.data)
